9f8a00145b6a66372e54a90539fc6628d731f0d6
Lucas M. Campos <lucas@lucasof.com>
2026-05-20 19:09:30 -0300

    chore: get port from env

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-  server: {
-    port: 3000
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const port = Number(env.PORT) || 5173
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss()
+    ],
+    server: { port },
+    preview: { port },
   }
 })
